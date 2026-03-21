bc2753e9c1af312ea21818caf3f9a001d966060b
Chahat Soni <chahatsoni2529@gmail.com>
2026-03-21 16:15:56 +0530

    Add program to swap two numbers

---
+++ cpp-learning-journey/basics/swapping_numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter the value of a and b: " << endl;
+    cin >> a >> b;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "Swapped values are: a = " << a << " b = " << b << endl;
+
+    return 0;
+}
