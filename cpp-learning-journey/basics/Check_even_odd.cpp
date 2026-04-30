592adad9abdcf8a59b885115393b3d2ed044dfe7
Chahat Soni <chahatsoni2529@gmail.com>
2026-04-30 22:22:40 +0530

    Add Check_even_odd.cpp to determine even or odd

---
+++ cpp-learning-journey/basics/Check_even_odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout << "Enter the number\n";
+    cin >> a;
+    if (a%2==0)
+    {
+        cout << "Even";
+    }
+    else{
+        cout << "Odd";
+    }
+
+    return 0;
+    
+}
