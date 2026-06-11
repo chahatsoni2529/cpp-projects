0ee9f7d6b03aba68c02c113d03abf1f07dc0f58e
Chahat Soni <chahatsoni2529@gmail.com>
2026-06-11 20:53:37 +0530

    Add program to reverse a string using pointers

---
+++ cpp-learning-journey/basics/reverse_string_using_pointers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[100];
+
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    char *start = str;
+    char *end = str + strlen(str) - 1;
+
+    while (start < end) {
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+
+    cout << "Reversed String: " << str;
+
+    return 0;
+}
